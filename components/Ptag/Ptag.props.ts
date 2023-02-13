5ab101a4cb3ff49c5b030f93f6337cb2c88951b2
Dima Ivanov <latrey.9451@gmail.com>
2023-02-13 09:43:31 +0300

    add p tag

--- components/Ptag/Ptag.props.ts
+++ components/Ptag/Ptag.props.ts
@@ -1,7 +1,7 @@
-import { ReactNode } from "react"
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
 
 
-export interface PtagProps {
+export interface PtagProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
     size?: 'small' | 'medium' | 'large'
     children: ReactNode
  }
--- components/Ptag/Ptag.tsx
+++ components/Ptag/Ptag.tsx
@@ -2,13 +2,14 @@ import { PtagProps } from "./Ptag.props"
 import s from './Ptag.module.scss'
 import cn from 'classnames'
 
-export const Ptag = ({size = 'medium', children}:PtagProps):JSX.Element=> {
+export const Ptag = ({size = 'medium', className, children, ...props}:PtagProps):JSX.Element=> {
     return (
-        <p className={cn(s.text, {
+        <p className={cn(s.text, className, {
             [s.small]: size === 'small',
             [s.medium]: size === 'medium',
             [s.large]: size === 'large',
-        })}>
+        })}
+        {...props}>
             {children}
         </p>
     )
